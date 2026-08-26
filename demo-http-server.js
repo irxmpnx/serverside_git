9d01e046d81890e521456dc79c57a7c516170724
irxmpnx <nannapas.mori@gmail.com>
2026-08-26 21:58:59 +0700

    Add /about and /contact routes

--- demo-http-server.js
+++ demo-http-server.js
@@ -4,7 +4,14 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+
+  if (req.url === '/about') {
+    res.end('About Page\n');
+  } else if (req.url === '/contact') {
+    res.end('Contact Page\n');
+  } else {
+    res.end('Hello, World!\n');
+  }
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
